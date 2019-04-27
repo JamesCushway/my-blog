e34b5643ac111f91636b151f17faac97b1b33ede
James Cushway <james@citytaps.org>
2019-04-27 20:29:30 +0200

    Revert "more analytics"
    This reverts commit 80ca5839ddb7fd75b294487865ea3beab0cf29cb.

--- src/main.js
+++ src/main.js
@@ -8,10 +8,10 @@ new Vue({
   router,
 }).$mount('#app')
 
-ga('set', 'page', router.currentRoute.path);
-ga('send', 'pageview');
+gtag('set', 'page', router.currentRoute.path);
+gtag('send', 'pageview');
 
 router.afterEach(( to, from ) => {
-  ga('set', 'page', to.path);
-  ga('send', 'pageview');
+  gtag('set', 'page', to.path);
+  gtag('send', 'pageview');
 });
